139d8bbdb8d0657b79af381ecc892e81d507ce97
kristof553 <kristofmolnar553@gmail.com>
2022-12-21 12:29:45 +0100

    implement getApi fetch

--- nava-hw-frontend/src/App.tsx
+++ nava-hw-frontend/src/App.tsx
@@ -2,15 +2,23 @@ import React, {ChangeEvent, FC, useEffect, useState} from 'react';
 
 const App: FC = () => {
 
+    const BASE_API_ENDPOINT = "/picture"
+
+    const [pictures, setPictures] = useState([])
+
+    const getPictures = async () => {
+        let data = await fetch(BASE_API_ENDPOINT)
+        return await data.json()
+    }
+
     useEffect(() => {
-        fetch("http://localhost:8080/")
-            .then(response => response.json())
-            .then(res => console.log(res))
+        getPictures()
+            .then(res => console.log(res));
     }, [])
 
   return (
    <div>
-        NAVA
+       {pictures}
    </div>
   );
 }
